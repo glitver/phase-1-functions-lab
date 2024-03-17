f027495b0b1c4ef754430263e7000cbb66ab801d
+ <+>
2024-03-17 17:36:34 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const headquarters = 42;
+    let distance = someValue - headquarters;
+    if (distance < 0) 
+        distance = -distance; 
+    return distance;
+}
+  function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+  }
+  function distanceTravelledInFeet(start, destination) {
+    const distance = destination - start;
+    return distance >= 0 ? distance * 264 : -distance * 264;
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance > 2500) 
+        return "cannot travel that far";
+    if (distance <= 400) 
+        return 0;
+    if (distance <= 2000) 
+        return (distance - 400) * 0.02;
+    return 25;
+}
